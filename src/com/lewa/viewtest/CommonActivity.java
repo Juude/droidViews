769182868a28b64e1034d74b8c48537e0de5c46c
jdsong <xsongjinde@lewatek.com>
2014-03-27 20:42:00 +0800

    refactor the lewa mock

--- src/com/lewa/viewtest/CommonActivity.java
+++ src/com/lewa/viewtest/CommonActivity.java
@@ -1,15 +1,24 @@
 package com.lewa.viewtest;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.ActionBar.LayoutParams;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemProperties;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+
+import java.lang.reflect.Constructor;
 
 
 public class CommonActivity extends Activity {
 
     public static final String KEY_FRAGMENT = "fragment";
+    public static final String KEY_VIEW = "view";
+    private static final String TAG = "CommonActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +30,32 @@ public class CommonActivity extends Activity {
         parent.setId(android.R.id.content);
         setContentView(parent);
         initActionbar();
-        setupFragment();
+        String clazz = getIntent().getStringExtra(KEY_FRAGMENT);
+        if(clazz != null) {
+            setupFragment();
+            return;
+        }
+        Log.e(TAG, "heool");
+        String viewClazz = getIntent().getStringExtra(KEY_VIEW);
+        setupView(parent, viewClazz);
+        
     }
     
+    private void setupView(ViewGroup parent, String viewClazz) {
+        Class<?> viewClass;
+        try {
+            viewClass = Class.forName(viewClazz);
+            Constructor<?> viewConstructor = viewClass.getConstructor(Context.class, FrameLayout.LayoutParams.class);
+            View v = (View)viewConstructor.newInstance(this, new FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, 
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+            parent.addView(v);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void initActionbar(){
         String title = getIntent().getStringExtra(Intent.EXTRA_TITLE);
         if(title != null)
--- src/com/lewa/viewtest/mock/MockUtils.java
+++ src/com/lewa/viewtest/mock/MockUtils.java
@@ -2,10 +2,34 @@ package com.lewa.viewtest.mock;
 
 import android.content.Context;
 import android.content.Intent;
-
+import android.os.Bundle;
 import com.lewa.viewtest.CommonActivity;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class MockUtils {
+    @Retention(RetentionPolicy.RUNTIME)
+    static public @interface Mock {
+    }
+    
+    public static String getString(Bundle bundle, String key, String def) {
+        String result = bundle.getString(key);
+        result = result == null ? def : result;
+        return result;
+    }
+    
+    public static int getInt(Bundle bundle, String key, int def) {
+        String str = bundle.getString(key);
+        int result = def;
+        try {
+            result = Integer.parseInt(str);
+        }
+        catch(Exception e) {
+        }
+        return result; 
+    }
+    
     public static void startFragment(Context context , String fragmentName) {
         Intent i = new Intent(context, CommonActivity.class);
         i.putExtra(CommonActivity.KEY_FRAGMENT, fragmentName);
