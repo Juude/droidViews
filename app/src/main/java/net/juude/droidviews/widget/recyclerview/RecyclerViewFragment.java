865a36c2841fa85f39a992bed969e30347b6a975
J.D.Song <juude.song@gmail.com>
2015-05-24 19:40:17 +0800

    add recyclerview

---
+++ app/src/main/java/net/juude/droidviews/widget/recyclerview/RecyclerViewFragment.java
@@ -0,0 +1,19 @@
+package net.juude.droidviews.widget.recyclerview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by juude on 15/5/24.
+ */
+public class RecyclerViewFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
