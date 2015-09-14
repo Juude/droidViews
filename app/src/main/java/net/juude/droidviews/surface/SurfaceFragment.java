e2ea6fe0a535e2ef67756c1906091ad238777c39
J.D.Song <juude.song@gmail.com>
2015-09-14 19:36:44 +0800

    test SurfaceFragment

--- app/src/main/java/net/juude/droidviews/surface/SurfaceFragment.java
+++ app/src/main/java/net/juude/droidviews/surface/SurfaceFragment.java
@@ -13,13 +13,24 @@ import net.juude.droidviews.R;
  * Created by juude on 15-8-21.
  */
 public class SurfaceFragment extends Fragment{
-
+    private DemoGLSurfaceView mDemoGLSurfaceView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_surface, null);
+        mDemoGLSurfaceView = (DemoGLSurfaceView) v.findViewById(R.id.demoGLSurfaceView);
         return v;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mDemoGLSurfaceView.onResume();
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        mDemoGLSurfaceView.onPause();
+    }
 }
