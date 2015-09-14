94956587b475177407fc587bf5f064c46016ee8f
J.D.Song <juude.song@gmail.com>
2015-09-14 19:52:58 +0800

    add Wallpaper

---
+++ app/src/main/java/net/juude/droidviews/libgdx/SnowWallpaper.java
@@ -0,0 +1,13 @@
+package net.juude.droidviews.libgdx;
+
+import com.badlogic.gdx.backends.android.AndroidLiveWallpaper;
+import com.badlogic.gdx.backends.android.AndroidLiveWallpaperService;
+
+/**
+ * Created by juude on 15-9-14.
+ */
+public class SnowWallpaper extends AndroidLiveWallpaper{
+    public SnowWallpaper(AndroidLiveWallpaperService service) {
+        super(service);
+    }
+}
