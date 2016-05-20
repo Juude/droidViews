8bc260d73b2e77f45839a0c09b4d430ad6501566
J.D.Song <juude.song@gmail.com>
2016-05-20 15:55:23 +0800

    add stetho demos

---
+++ stethdemos/src/main/java/net/juude/stethdemos/StethoApplication.java
@@ -0,0 +1,31 @@
+package net.juude.stethdemos;
+
+import android.app.Application;
+
+import com.facebook.stetho.InspectorModulesProvider;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.inspector.protocol.ChromeDevtoolsDomain;
+import com.facebook.stetho.rhino.JsRuntimeReplFactoryBuilder;
+
+/**
+ * Created by juude on 16/5/19.
+ */
+public class StethoApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initialize(Stetho.newInitializerBuilder(StethoApplication.this)
+                .enableWebKitInspector(new InspectorModulesProvider() {
+                    @Override
+                    public Iterable<ChromeDevtoolsDomain> get() {
+                        return new Stetho.DefaultInspectorModulesBuilder(StethoApplication.this).runtimeRepl(
+                                new JsRuntimeReplFactoryBuilder(StethoApplication.this)
+                                        // Pass to JavaScript: var foo = "bar";
+                                        .addVariable("foo", "bar")
+                                        .build()
+                        ).finish();
+                    }
+                })
+                .build());
+    }
+}
