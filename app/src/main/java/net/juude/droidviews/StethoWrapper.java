6000c2b754e200007ba6ded8b949cbd83f28a469
J.D.Song <juude.song@gmail.com>
2016-08-08 19:27:45 +0800

    update gitignore

--- app/src/main/java/net/juude/droidviews/StethoWrapper.java
+++ app/src/main/java/net/juude/droidviews/StethoWrapper.java
@@ -1,9 +1,7 @@
 package net.juude.droidviews;
 
 import android.app.Application;
-import android.util.Log;
 
-import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.stetho.DumperPluginsProvider;
 import com.facebook.stetho.InspectorModulesProvider;
 import com.facebook.stetho.Stetho;
@@ -11,21 +9,26 @@ import com.facebook.stetho.dumpapp.DumperPlugin;
 import com.facebook.stetho.inspector.protocol.ChromeDevtoolsDomain;
 import com.facebook.stetho.rhino.JsRuntimeReplFactoryBuilder;
 
-public class DroidViewsApplication extends Application{
-    private static final String TAG = "DroidViewsApplication";
-    private static DroidViewsApplication sInstance;
+import java.util.ArrayList;
+import java.util.List;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Fresco.initialize(this);
-        sInstance = this;
-        Stetho.initialize(Stetho.newInitializerBuilder(DroidViewsApplication.this)
+/**
+ * Created by juude on 16/8/5.
+ */
+public class StethoWrapper {
+
+    private static List<DumperPlugin> sDumperList = new ArrayList<>();
+    static {
+       // sDumperList.add(new AlarmPlugin());
+    }
+
+    public void onCreate(final Application application) {
+        Stetho.initialize(Stetho.newInitializerBuilder(application)
                 .enableWebKitInspector(new InspectorModulesProvider() {
                     @Override
                     public Iterable<ChromeDevtoolsDomain> get() {
-                        return new Stetho.DefaultInspectorModulesBuilder(DroidViewsApplication.this).runtimeRepl(
-                                new JsRuntimeReplFactoryBuilder(DroidViewsApplication.this)
+                        return new Stetho.DefaultInspectorModulesBuilder(application).runtimeRepl(
+                                new JsRuntimeReplFactoryBuilder(application)
                                         // Pass to JavaScript: var foo = "bar";
                                         .addVariable("foo", "bar")
                                         .build()
@@ -35,25 +38,9 @@ public class DroidViewsApplication extends Application{
                 .enableDumpapp(new DumperPluginsProvider() {
                     @Override
                     public Iterable<DumperPlugin> get() {
-                        return null;
+                        return sDumperList;
                     }
                 })
                 .build());
-    }
-
-    public static DroidViewsApplication getInstance() {
-        return sInstance;
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        Log.d(TAG, "onLowMemory");
-    }
-
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-        Log.d(TAG, "onTerminate");
     }
 }
