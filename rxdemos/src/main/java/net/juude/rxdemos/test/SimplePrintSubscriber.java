ffd8bde36d5a46fd01042c2a94fb596f46502e7b
J.D.Song <juude.song@gmail.com>
2016-08-09 20:21:59 +0800

    optimize observerable

--- rxdemos/src/main/java/net/juude/rxdemos/test/SimplePrintSubscriber.java
+++ rxdemos/src/main/java/net/juude/rxdemos/test/SimplePrintSubscriber.java
@@ -6,19 +6,30 @@ import rx.Subscriber;
  * Created by sjd on 16/8/9.
  */
 public class SimplePrintSubscriber extends Subscriber{
+
+    private String name = "noname";
+
+    public SimplePrintSubscriber() {
+
+    }
+
+    public SimplePrintSubscriber(String name) {
+        this.name = name;
+    }
+
     @Override
     public void onCompleted() {
-        System.out.println(" onCompleted : " );
+        System.out.println(this.name + " onCompleted : " );
     }
 
     @Override
     public void onError(Throwable e) {
-        System.out.println(" onError : " + e);
+        System.out.println(this.name + " onError : " + e);
     }
 
     @Override
     public void onNext(Object o) {
-        System.out.println(" onNext : " + o);
+        System.out.println(this.name + " onNext : " + o);
     }
 
 }
--- rxdemos/src/test/java/net/juude/rxdemos/TestMultipleObservers.java
+++ rxdemos/src/test/java/net/juude/rxdemos/TestMultipleObservers.java
@@ -1,16 +1,22 @@
 package net.juude.rxdemos;
 
+import junit.framework.Assert;
+
 import net.juude.rxdemos.test.SimplePrintSubscriber;
 import net.juude.rxdemos.test.UpdateableOnSubscribe;
 
 import org.junit.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 import rx.Subscription;
 import rx.functions.Action1;
+import rx.observers.TestSubscriber;
+import rx.subjects.AsyncSubject;
 import rx.subjects.PublishSubject;
+import rx.subjects.ReplaySubject;
 
 /**
  * Created by juude on 16/8/9.
@@ -43,11 +49,11 @@ public class TestMultipleObservers {
                 observable.onNext("next  : " + System.currentTimeMillis());
             }
         });
-        try {
-            Thread.sleep(33333);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(33333);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Test
@@ -61,5 +67,37 @@ public class TestMultipleObservers {
         subject.subscribe(new SimplePrintSubscriber());
         subject.onNext("three");
         subject.onCompleted();
+    }
+
+    @Test
+    public void testAsyncSubject() {
+        AsyncSubject<Object> subject = AsyncSubject.create();
+        subject.subscribe(new SimplePrintSubscriber("1"));
+        subject.onNext("one");
+        subject.onNext("two");
+        subject.onNext("three");
+
+        // observer will receive "three" as the only onNext event.
+        AsyncSubject<Object> subject2 = AsyncSubject.create();
+        subject2.subscribe(new SimplePrintSubscriber("2"));
+        subject2.onNext("one");
+        subject2.onNext("two");
+        subject2.onNext("three");
+        subject2.onCompleted();
+    }
+
+    @Test
+    public void testReplaySubject() {
+        ReplaySubject<String> replaySubject = ReplaySubject.create();
+        replaySubject.onNext("22");
+        replaySubject.onNext("33");
+        replaySubject.subscribe(new SimplePrintSubscriber("replaySubject"));
+        replaySubject.onNext("3333");
+        replaySubject.onCompleted();
+        TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+        replaySubject.subscribe(testSubscriber);
+        testSubscriber.assertNoErrors();
+        List<String> results = testSubscriber.getOnNextEvents();
+        Assert.assertTrue(results.size() == 3);
     }
 }
