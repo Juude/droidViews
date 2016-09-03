5634cab5cc2d8722fa3bb466500b622ca80ca74e
J.D.Song <juude.song@gmail.com>
2016-09-03 14:28:36 +0800

    add onerror resume next

--- rxdemos/src/test/java/net/juude/rxdemos/ErrorHandlingTest.java
+++ rxdemos/src/test/java/net/juude/rxdemos/ErrorHandlingTest.java
@@ -1,53 +1,27 @@
 package net.juude.rxdemos;
-import net.juude.rxdemos.test.SimplePrintSubscriber;
-
+import junit.framework.Assert;
 import org.junit.Test;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action0;
 import rx.functions.Func1;
 import rx.functions.Func2;
-import rx.subjects.PublishSubject;
+import rx.observers.TestSubscriber;
 import top.perf.utils.LogUtils;
-import top.perf.utils.timer.TimerUtils;
 
 /**
  * Created by juude on 16/8/24.
  */
 public class ErrorHandlingTest {
 
-    public static final String TAG = "ErrorHandlingTest";
-
-
-    //@Test
-    public void testRetrSubject() {
-        PublishSubject<Integer> publishSubject = PublishSubject.create();
-        publishSubject
-                .asObservable()
-                .retry()
-                .subscribe(new SimplePrintSubscriber());
-        publishSubject.onNext(100);
-        publishSubject.onError(new Exception("dd"));
-        publishSubject.onNext(233);
-    }
-
-    public void testRetryWhexn() {
-        //        Observable.create((new Subscriber<? super String> s) -> {
-//            System.out.println("subscribing");
-//            s.onError(new RuntimeException("always fails"));
-//        }).retryWhen(attempts -> {
-//            return attempts.zipWith(Observable.range(1, 3), (n, i) -> i).flatMap(i -> {
-//                System.out.println("delay retry by " + i + " second(s)");
-//                return Observable.timer(i, TimeUnit.SECONDS);
-//            });
-//        }).toBlocking().forEach(System.out::println);
-
-    }
-
-    //@Test
+    @Test
     public void testRetry() {
+        final AtomicInteger atomicInteger = new AtomicInteger(0);
         Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
@@ -55,24 +29,16 @@ public class ErrorHandlingTest {
                 subscriber.onError(new Error("ddd"));
             }
         })
-        .retry()
-        .subscribe(new Subscriber<String>() {
-
-             @Override
-             public void onCompleted() {
-                 System.out.println("complete");
-             }
-
-             @Override
-             public void onError(Throwable e) {
-                 System.out.println("onError" + LogUtils.getStackTraceString(e));
-             }
-
-             @Override
-             public void onNext(String s) {
-                 System.out.println("onNext : " + s);
-             }
-         });
+        .doOnSubscribe(new Action0() {
+            @Override
+            public void call() {
+                atomicInteger.incrementAndGet();
+            }
+        })
+        .retry(2)
+        .toBlocking()
+        .subscribe(new TestSubscriber<String>());
+        Assert.assertTrue(atomicInteger.intValue() == 3);
     }
 
     @Test
@@ -93,7 +59,7 @@ public class ErrorHandlingTest {
         .retryWhen(new Func1<Observable<? extends Throwable>, Observable<?>>() {
             @Override
             public Observable<?> call(Observable<? extends Throwable> observable) {
-                return observable.zipWith(Observable.range(1, 1000), new Func2<Throwable, Integer, Integer>() {
+                return observable.zipWith(Observable.range(1, 3), new Func2<Throwable, Integer, Integer>() {
                     @Override
                     public Integer call(Throwable throwable, Integer integer) {
                         return integer;
@@ -125,6 +91,7 @@ public class ErrorHandlingTest {
                 return observable;
             }
         })
+        .toBlocking()
         .subscribe(new Subscriber<String>() {
 
             @Override
@@ -142,6 +109,46 @@ public class ErrorHandlingTest {
                 System.out.println("onNext : " + s);
             }
         });
-        TimerUtils.sleepIgnoreExceptions(10000);
+    }
+
+    @Test
+    public void testOnErrorReturn() {
+        final AtomicInteger atomicInteger = new AtomicInteger(0);
+        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                subscriber.onError(new Exception(String.valueOf(atomicInteger.getAndIncrement())));
+            }
+        })
+        .onErrorReturn(new Func1<Throwable, Integer>() {
+            @Override
+            public Integer call(Throwable throwable) {
+                return -1;
+            }
+        })
+        .subscribe(testSubscriber);
+        org.junit.Assert.assertEquals(testSubscriber.getOnNextEvents().get(0).intValue(), -1);
+    }
+
+    @Test
+    public void testOnExceptionResumeNext() {
+        final AtomicInteger atomicInteger = new AtomicInteger(0);
+        TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                subscriber.onNext(-1);
+                subscriber.onError(new Exception(String.valueOf(atomicInteger.getAndIncrement())));
+            }
+        })
+        .onErrorResumeNext(Observable.just(1,3,5))
+        .subscribe(testSubscriber);
+        List<Integer> nextList = testSubscriber.getOnNextEvents();
+        Assert.assertTrue(nextList.size() == 4);
+        Assert.assertTrue(nextList.get(0) == -1);
+        Assert.assertTrue(nextList.get(1) == 1);
+        Assert.assertTrue(nextList.get(2) == 3);
+        Assert.assertTrue(nextList.get(3) == 5);
     }
 }
